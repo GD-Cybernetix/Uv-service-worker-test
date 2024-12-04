2c1d6599a6ac3bb89ef65f3154ade1631ac150e0
GD Cyber <154467176+GD-Cybernetix@users.noreply.github.com>
2024-12-04 13:30:42 +0000

    Delete static/assets/js directory

--- static/assets/js/functions.js
+++
@@ -1,9 +0,0 @@
-function discord () {
-
-    window.navigator.serviceWorker.register('/sw.js', {
-        scope: __uv$config.prefix
-    }).then(() => {
-        location.href=__uv$config.prefix + __uv$config.encodeUrl('https://discord.com');
-    });
-
-}
